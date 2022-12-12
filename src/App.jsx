0b3c5067872c569820f9ea081f3dbbd72c94f0a4
YannSTEFANUTTI <stefanutti.yann@gmail.com>
2022-12-12 13:58:57 +0100

    Date filter OK

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,36 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import CardModel from "./components/CardModel";
-import data from "./assets/data";
 import TitleBar from "./components/TitleBar";
 
 function App() {
   const [selectReason, setSelectReason] = useState("");
-    const [datas, setDatas] = useState([]);
+  const [selectDate, setSelectDate] = useState("");
+  const [datas, setDatas] = useState([]);
   const apiURL =
     "https://opendata.bordeaux-metropole.fr/api/records/1.0/search/?dataset=previsions_pont_chaban&q=&rows=10000&sort=date_passage";
-  
 
-   useEffect(() => {
-     const url = `${apiURL}`;
-     axios.get(url).then((response) => {
-       setDatas(response.data);
-     });
-   }, []);
+  useEffect(() => {
+    const url = `${apiURL}`;
+    axios.get(url).then((response) => {
+      setDatas(response.data);
+    });
+  }, []);
 
   return (
     <div className="App">
-      <TitleBar setSelectReason={setSelectReason} selectReason={selectReason} />
+      <TitleBar
+        setSelectReason={setSelectReason}
+        setSelectDate={setSelectDate}
+      />
       <div className="allCards">
         {datas.records &&
           datas.records
 
             .filter((el) => !selectReason || el.fields.bateau === selectReason)
+            .filter(
+              (el) => !selectDate || el.fields.date_passage === selectDate
+            )
 
             .map((el) => (
               <CardModel
--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import data from "../assets/data";
+import axios from "axios";
 
-const TitleBar = ({ selectReason, setSelectReason }) => {
-  const allReasons = data.map((el) => el.fields.bateau).sort();
-  const allReasonsOnce = Array.from(new Set(allReasons));
 
+const TitleBar = ({ selectReason, setSelectReason, setSelectDate }) => {
   const [reasonFilter, setReasonFilter] = useState([]);
+  const [datas, setDatas] = useState([]);
+  const apiURL =
+    "https://opendata.bordeaux-metropole.fr/api/records/1.0/search/?dataset=previsions_pont_chaban&q=&rows=10000&sort=date_passage";
 
-  /*  useEffect(() => {
-   //const url = `${apiURL}/cupcakes`;
-   axios.get(data).then((response) => {
-    
-     setReasonFilter(response.data);
-   });
- }, []); */
+  useEffect(() => {
+    const url = `${apiURL}`;
+    axios.get(url).then((response) => {
+      setDatas(response.data.records);
+    });
+  }, []);
 
   const reasonSelection = (e) => {
     setSelectReason(e.target.value);
   };
+  const DateSelection = (e) => {
+    setSelectDate(e.target.value);
+  };
+  const allReasons = datas.map((el) => el.fields.bateau).sort();
+  const allReasonsOnce = Array.from(new Set(allReasons));
+
+  const allDates = datas.map((el) => el.fields.date_passage).sort();
+  const allDatesOnce = Array.from(new Set(allDates));
 
   return (
     <div className="titleBar">
@@ -38,8 +46,18 @@ const TitleBar = ({ selectReason, setSelectReason }) => {
           Filtrer par Bateau ou événement <br />
           <select onChange={reasonSelection}>
             <option value="">---</option>
-            {data &&
+            {datas &&
               allReasonsOnce.map((el) => <option value={el}>{el}</option>)}
+          </select>
+        </label>
+      </form>
+      <form id="formFilter">
+        <label>
+          Filtrer par date <br />
+          <select onChange={DateSelection}>
+            <option value="">---</option>
+            {datas &&
+              allDatesOnce.map((el) => <option value={el}>{el}</option>)}
           </select>
         </label>
       </form>
